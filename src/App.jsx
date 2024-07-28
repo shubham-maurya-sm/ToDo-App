f0ddb3b8648d4c14d9d4784bd51374703aa59361
Shubham Maurya <160427804+shubham-maurya-sm@users.noreply.github.com>
2024-07-28 10:03:06 +0530

    rectified errors

--- src/App.jsx
+++ src/App.jsx
@@ -55,9 +55,7 @@ function App() {
         To Do List Application
       </h1> */}
 
-      <div className="bg-[#2d3439] min-h-screen py-8"
-        
-        }}>
+      <div className="bg-[#2d3439] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
